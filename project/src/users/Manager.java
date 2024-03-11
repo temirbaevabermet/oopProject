6ec2370a75831981fe45af243413cccf8bce3355
temirbaevabermet <temirbaevabermet4@gmail.com>
2024-03-11 23:39:37 +0600

    added code for manager which shows count of vouchers; coverage area

--- project/src/users/Manager.java
+++ project/src/users/Manager.java
@@ -10,20 +10,12 @@ public class Manager {
         System.out.println("1. Show list of employees;\n" +
                 "2. Show to-do list;\n" +
                 "3. Show a list of instructions to employees;\n" +
-                "4. Show a list of all coverage areas:\n" +
-                "• Show customer coverage area for Bishkek\n" +
-                "• Show customer coverage area for Talas\n" +
-                "• Show customer coverage area for Jalal-Abad\n" +
-                "• Show customer coverage area for Osh\n" +
-                "• Show customer coverage area for Naryn\n" +
-                "• Show customer coverage area for Issyk-Kul\n" +
-                "• Show customer coverage area for Batken\n" +
-                "5. Show the amount of vouchers:\n" +
-                "• Show total amount\n" +
-                "• Show the amount for vouchers\n" +
+                "4. Show a list of all coverage areas;\n" +
+                "5. Show the amount of vouchers;\n" +
                 "6. Exit;");
     }
     public String getChoice(){
+        ManagerList managerList = new ManagerList();
         String choice = "null";
         String numberRequest = "Please dial the menu number to work with the program:";
         System.out.print(numberRequest);
@@ -45,42 +37,11 @@ public class Manager {
                     "3) Show the building to new employees;\n" +
                     "4) Print monthly report;";
         }
-        else if(getchoice.equals("4.1")){
-            choice = "Coverage percentage in Bishkek: 35%; " +
-                    "Number of clients: 700k people;";
+        else if(getchoice.equals("4")){
+            choice = managerList.coverageArea();
         }
-        else if(getchoice.equals("4.2")){
-            choice = "Coverage percentage in Talas: 15%; " +
-                    "Number of clients: 300k people;";
-        }
-        else if(getchoice.equals("4.3")){
-            choice = "Coverage percentage in Jalal-Abad: 7%; " +
-                    "Number of clients: 140k people;";
-        }
-        else if(getchoice.equals("4.4")){
-            choice = "Coverage percentage in Osh: 25%; " +
-                    "Number of clients: 500k people;";
-        }
-        else if(getchoice.equals("4.5")){
-            choice = "Coverage percentage in Naryn: 5%; " +
-                    "Number of clients: 100k people;";
-        }
-        else if(getchoice.equals("4.6")){
-            choice = "Coverage percentage in Issyk-Kul: 8%; " +
-                    "Number of clients: 160k people;";
-        }
-        else if(getchoice.equals("4.7")){
-            choice = "Coverage percentage in Batken: 5%; " +
-                    "Number of clients: 100k people;";
-        }
-        else if(getchoice.equals("5.1")){
-            choice = "Total amount of vouchers: 100;";
-        }
-        else if(getchoice.equals("5.2")){
-            choice = "o For 1st quarter: 40;\n" +
-                    "o For the 2nd quarter: 25;\n" +
-                    "o For the 3rd quarter: 15;\n" +
-                    "o For the 4th quarter: 20;";
+        else if(getchoice.equals("5")){
+            choice = managerList.vouchers();
         }
         else if(getchoice.equals("6")){
             choice = "The program is over, we look forward to your return!";
---
+++ project/src/users/ManagerList.java
@@ -0,0 +1,79 @@
+package users;
+
+import java.util.Scanner;
+
+public class ManagerList {
+    Scanner scanner = new Scanner(System.in);
+    public String coverageArea(){
+        System.out.println("1. Show customer coverage area for Bishkek\n" +
+                "2. Show customer coverage area for Talas\n" +
+                "3. Show customer coverage area for Jalal-Abad\n" +
+                "4. Show customer coverage area for Osh\n" +
+                "5. Show customer coverage area for Naryn\n" +
+                "6. Show customer coverage area for Issyk-Kul\n" +
+                "7. Show customer coverage area for Batken");
+        System.out.print("Choose one category:");
+        String chooseArea = scanner.next();
+        String choosedArea = "null";
+        if(chooseArea.equals("1")){
+            choosedArea = "Coverage percentage in Bishkek: 35%; " +
+                    "Number of clients: 700k people;";
+        }
+        else if(chooseArea.equals("2")){
+            choosedArea = "Coverage percentage in Talas: 15%; " +
+                    "Number of clients: 300k people;";
+        }
+        else if(chooseArea.equals("3")){
+            choosedArea = "Coverage percentage in Jalal-Abad: 7%; " +
+                    "Number of clients: 140k people;";
+        }
+        else if(chooseArea.equals("4")){
+            choosedArea = "Coverage percentage in Osh: 25%; " +
+                    "Number of clients: 500k people;";
+        }
+        else if(chooseArea.equals("5")){
+            choosedArea = "Coverage percentage in Naryn: 5%; " +
+                    "Number of clients: 100k people;";
+        }
+        else if(chooseArea.equals("6")){
+            choosedArea = "Coverage percentage in Issyk-Kul: 8%; " +
+                    "Number of clients: 160k people;";
+        }
+        else if(chooseArea.equals("7")){
+            choosedArea = "Coverage percentage in Batken: 5%; " +
+                    "Number of clients: 100k people;";
+        }
+        return choosedArea;
+    }
+    public String vouchers(){
+        System.out.println("1.Show total amount\n" +
+                "2.Show the amount for vouchers");
+        System.out.print("Choose one category: ");
+        String chooseVoucher = scanner.next();
+        String choosedVoucher = "null";
+        if(chooseVoucher.equals("1")){
+            choosedVoucher = "Total amount of vouchers: 100;";
+        }
+        else if(chooseVoucher.equals("2")){
+            System.out.println("1.For 1st quarter;\n" +
+                    "2. For the 2nd quarter;\n" +
+                    "3. For the 3rd quarter;\n" +
+                    "4. For the 4th quarter;");
+            System.out.print("Choose one quarter:");
+            String chooseQuarter = scanner.next();
+            if(chooseQuarter.equals("1")){
+                choosedVoucher = "For 1st quarter: 40;";
+            }
+            else if(chooseQuarter.equals("2")){
+                choosedVoucher = "For the 2nd quarter: 25;";
+            }
+            else if(chooseQuarter.equals("3")){
+                choosedVoucher = "For the 3rd quarter: 15;";
+            }
+            else if(chooseQuarter.equals("4")){
+                choosedVoucher = "For the 4th quarter: 20;";
+            }
+        }
+        return choosedVoucher;
+    }
+}
